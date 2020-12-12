e6a1c588f76beb9ba9581a130a599bd5b107c470
Max <max.ronald9@gmail.com>
2020-12-12 23:42:03 +0300

    Add Polina's seventh lab

---
+++ Polina/SeventhLab/SeventhLab/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeventhLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string str)
+        {
+            if (str != null)
+            {
+                var symbolsMassive = str.Split(new char[] { ' ' });
+                var statistic = new Dictionary<string, int>();
+                foreach (var symbol in symbolsMassive)
+                {
+                    if (statistic.ContainsKey(symbol))
+                    {
+                        statistic[symbol]++;
+                    }
+                    else
+                    {
+                        statistic.Add(symbol, 1);
+                    }
+                }
+                string response = "";
+                foreach (var data in statistic)
+                {
+                    response += data.Key + " - " + data.Value + "; ";
+                }
+                ViewData["Response"] = response;
+            }
+            return View();
+        }
+    }
+}
